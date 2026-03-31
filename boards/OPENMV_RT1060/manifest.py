a533f7cf064d5e551963606e6b4e12071bc62efd
Praful Mathur <praful.mathur@gmail.com>
2026-03-31 11:01:57 -0700

    OPENMV_RT1060: Add manifest for microdot build.
    Signed-off-by: Praful Mathur <praful.mathur@gmail.com>

--- boards/OPENMV_RT1060/manifest.py
+++ boards/OPENMV_RT1060/manifest.py
@@ -25,7 +25,7 @@ require("ssl")
 require("ntptime")
 require("webrepl")
 freeze ("$(OMV_LIB_DIR)/", "rpc.py")
-freeze ("$(OMV_LIB_DIR)/", "rtsp.py")
+# freeze ("$(OMV_LIB_DIR)/", "rtsp.py")  # Use device/rtsp.py instead
 freeze ("$(OMV_LIB_DIR)/", "mqtt.py")
 freeze ("$(OMV_LIB_DIR)/", "requests.py")
 
@@ -38,6 +38,25 @@ freeze ("$(OMV_LIB_DIR)/", "mutex.py")
 # Libraries
 require("ml", library="openmv-lib")
 include("$(MPY_DIR)/extmod/asyncio")
+
+# GroupGets device stack.
+GROUPGETS_DIR = "../../../groupgets"
+freeze(GROUPGETS_DIR + "/frameworks/microdot/src", "microdot")
+freeze(GROUPGETS_DIR + "/device", "boson.py")
+freeze(GROUPGETS_DIR + "/device", "boson_info.py")
+freeze(GROUPGETS_DIR + "/device", "schema.py")
+freeze(GROUPGETS_DIR + "/device", "openmv_server.py")
+freeze(GROUPGETS_DIR + "/device", "main.py")
+freeze(GROUPGETS_DIR + "/device", "rtsp.py")
+freeze(GROUPGETS_DIR + "/device", "state.py")
+freeze(GROUPGETS_DIR + "/device", "persistence.py")
+freeze(GROUPGETS_DIR + "/device", "templates.py")
+freeze(GROUPGETS_DIR + "/device", "routes_auth.py")
+freeze(GROUPGETS_DIR + "/device", "routes_stream.py")
+freeze(GROUPGETS_DIR + "/device", "routes_settings.py")
+freeze(GROUPGETS_DIR + "/device", "routes_wifi.py")
+freeze(GROUPGETS_DIR + "/device", "wifi_crypto.py")
+freeze(GROUPGETS_DIR, "static")
 
 # Boot script
 freeze ("$(OMV_LIB_DIR)/", "_boot.py")
